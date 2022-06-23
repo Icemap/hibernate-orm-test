3604843ae35b56f0c8c8dce9e506d02950b818d6
Icemap <qizhi.wang@pingcap.com>
2022-06-23 18:11:16 +0800

    [Update] init.sql

--- init.sql
+++ init.sql
@@ -1,7 +1,12 @@
-SET GLOBAL tidb_enable_noop_functions = 1;
-
 CREATE USER IF NOT EXISTS 'hibernate_orm_test' IDENTIFIED BY 'hibernate_orm_test';
 GRANT ALL ON *.* TO 'hibernate_orm_test';
 FLUSH PRIVILEGES;
 
-CREATE DATABASE IF NOT EXISTS hibernate_orm_test;
+set global tidb_enable_window_function = off;
+set global tidb_enable_noop_functions = on;
+set global tidb_txn_mode = pessimistic;
+set global time_zone = UTC;
+set global tidb_enable_async_commit = 0;
+set global tidb_enable_1pc = 0;
+
+CREATE DATABASE IF NOT EXISTS hibernate_orm_test collate=utf8mb4_general_ci;
